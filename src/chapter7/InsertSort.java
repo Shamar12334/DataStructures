5a5476652cc68942b8bc2e25ade7a3b7ede99cfa
shamar12334 <you@example.com>
2025-03-19 17:52:13 -0400

    created a file for shellsort

--- src/chapter7/InsertSort.java
+++ src/chapter7/InsertSort.java
@@ -1,4 +1,17 @@
 package chapter7;
 
 public class InsertSort {
+    //simplest insertion sort
+    // a an array of comparable items
+    public static <T extends Comparable <? super T>> void insertionSort(T [] a)
+    {
+        int j;
+        for(int p = 1; p < a.length; p++)
+        {
+            T temp=a[p];
+            for(j=p; j> 0 && temp.compareTo(a[j-1]) < 0; j++)
+                a[j] = a[j-1];
+            a[j] =temp;
+        }
+    }
 }
